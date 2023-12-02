4d17df8d4df262dd37218d784af57dfd2528af1b
Dylan-Dipasupil <96040584+Dylan-Dipasupil@users.noreply.github.com>
2023-12-02 13:23:21 -0800

    2023day1

--- dec1.py
+++ dec1.py
@@ -1,56 +1,107 @@
-f = open("inputs/dec1.txt", "r").read()
+from collections import defaultdict 
+f = open("inputs/dec1.txt", "r").readlines()
 
 # part 1
 """
-Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
+You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
 
-An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
 
-The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
-
-For example:
-
-    (()) and ()() both result in floor 0.
-    ((( and (()(()( both result in floor 3.
-    ))((((( also results in floor 3.
-    ()) and ))( both result in floor -1 (the first basement level).
-    ))) and )())()) both result in floor -3.
-
-To what floor do the instructions take Santa?
-"""
-def part1(text: str) -> None:
-    floor = 0
-    for ch in text:
-        if ch == "(":
-            floor += 1
-        else:
-            floor -= 1
-    return floor
-
-"""
-Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 
 For example:
 
-    ) causes him to enter the basement at character position 1.
-    ()()) causes him to enter the basement at character position 5.
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
 
 """
+def parse_line(line: str) -> int:
+    first_digit = ""
+    last_digit = ""
+    seen_first = False
+    for ch in line:
+        if ch.isdigit():
+            if not seen_first:
+                first_digit = ch
+                seen_first = True
+                last_digit = ch
+            else:
+                last_digit = ch
+    
+    if not first_digit:
+        return 0
+    return int(first_digit + last_digit)
 
-def part2(text: str) -> None:
-    floor = 0
-    for i in range(len(text)):
-        if text[i] == "(":
-            floor += 1
-        else:
-            floor -= 1
+def part1(file: list) -> None:
+    sum = 0
+    for line in file:
+        sum += parse_line(line)
+    return sum
+
+
+
+
+"""
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+"""
+def parse_line2(line: str) -> str:
+    if not line:
+        return ""
+    
+    for idx, ch in enumerate(line):
+        if ch.isdigit():
+            return ch + parse_line2(line[idx + 1:])
+        if ch == "o" and line.find("one") == idx:
+            return "1" + parse_line2(line[idx + 2:])
+        if ch == "t":
+            if line.find("two") == idx:
+                return "2" + parse_line2(line[idx + 2:])
+            if line.find("three") == idx:
+                return "3" + parse_line2(line[idx + 4:])
+        if ch == "f":
+            if line.find("four") == idx:
+                return "4" + parse_line2(line[idx + 3:])
+            if line.find("five") == idx:
+                return "5" + parse_line2(line[idx + 3:])
+        if ch == "s":
+            if line.find("six") == idx:
+                return "6" + parse_line2(line[idx + 2:])
+            if line.find("seven") == idx:
+                return "7" + parse_line2(line[idx + 4:])
+        if ch == "e" and line.find("eight") == idx:
+            return "8" + parse_line2(line[idx + 4:])
+        if ch == "n" and line.find("nine") == idx:
+            return "9" + parse_line2(line[idx + 3:])
+        
+    return ""
+
             
-        if floor == -1:
-            return i + 1
 
 
-endFloor = part1(f)
-basementPosition = part2(f)
-print(f"Part 1 End Floor: {endFloor}")
-print(f"Part 2 Basement Position: {basementPosition}")          
-          
+def part2(file: str) -> None:
+    sum = 0
+    for line in file:
+        new_line = parse_line2(line)
+        sum += parse_line(new_line)
+    return sum
+        
+
+part1res = part1(f)
+part2res = part2(f)
+print(f"Part 1 Result: {part1res}")
+print(f"Part 2 result: {part2res}")          
